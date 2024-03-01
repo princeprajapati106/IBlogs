402e2d6237c53ad9cd48ec9a28d63d99e0b4e818
princeprajapati106 <prjptprince106@gmail.com>
2024-03-01 16:17:53 +0530

    Add Website <Modified>

---
+++ IBlogs/Controllers/Website/WebSiteLogin/WebSite_LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IBlogs.Controllers.Website.WebSiteLogin
+{
+    public class WebSite_LoginConstroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
